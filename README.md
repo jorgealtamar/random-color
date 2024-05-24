4f5b3872166eeb14f5891d5cb8c4f8198dbcd57f
jorkaltamarjimenez <jorkaltamarjimenez@gmail.com>
2024-05-24 22:20:02 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Generador de Colores Aleatorios
+
+Este es un proyecto simple de una aplicación web que genera colores aleatorios en formato hexadecimal y los muestra en pantalla.
+
+## Funcionalidades
+
+- Genera colores aleatorios en formato hexadecimal.
+- Muestra el color generado en pantalla.
+- Permite cambiar el color haciendo clic en un botón.
+
+## Cómo usar
+
+1. Clona este repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador web.
+3. Haz clic en el botón "Cambiar color" para generar un nuevo color aleatorio.
+
+## Tecnologías utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT. Consulta el archivo [LICENSE](LICENSE) para más detalles.
---
+++ main.js
@@ -0,0 +1,25 @@
+// Add JS here
+// Seleccionar los elementos del DOM.
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+
+// Generar un color hexadecimal aleatorio.
+function generarColorHexAleatorio() {
+  let digitos = '0123456789ABCDEF'; // 16 opciones.
+  let color = '#'; // El color inicia con #.
+
+  for (let i = 0; i < 6; i++) {
+    // Escoger uno de los 16 digitos posibles.
+    let indiceAleatorio = Math.floor(Math.random() * 16);
+    // Agregarlo al codigo del color.
+    color += digitos[indiceAleatorio];
+  }
+  return color;
+}
+
+// Evento para cambiar color del fondo.
+boton.addEventListener('click', function() {
+  let colorAleatorio = generarColorHexAleatorio();
+  document.body.style.backgroundColor = colorAleatorio;
+  color.textContent = colorAleatorio;
+});
